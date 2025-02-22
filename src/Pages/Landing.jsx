9ca0093b96ae5a1ac0ffe0d43f6c77e86fb9e472
akghosh111 <ghoshkiran2017@gmail.com>
2025-02-22 18:51:58 +0530

    added hero section

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,8 +1,23 @@
 export const Landing = () => {
     return (
-        <div>
-            <h1>Flosync</h1>
-        </div>
+        <>
+            <section id="home" className="pt-24 min-h-screen flex items-center justify-center">
+                <div className="container mx-auto px-6">
+                    <div className="max-w-4xl mx-auto text-center">
+                        <h1 className="md:text-4xl"> Track Smarter, Feel Better, Own Your Cycle </h1>
+                        <br />
+                        <p className="md:text-xl">AI-powered menstrual insights, mood tracking, and a safe space to connect—because your cycle deserves more than just a calendar.</p>
+                    </div>
+                    <div className="mx-auto text-center pt-6 flex justify-center space-x-4">
+                        <button className="btn-primary">Get Started Free</button>
+                        <button className="btn-secondary">See How It Works</button>
+                    </div>
+
+                </div>
+                
+
+            </section>
+        </>
 
     )
 }
